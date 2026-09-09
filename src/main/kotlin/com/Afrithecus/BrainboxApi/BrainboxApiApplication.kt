50c353ee60ab3d0b0ca9eff86d6e118577da025e
Afrithecus <eeisenman7@gmail.com>
2026-09-09 19:26:40 +0300

    initial commit: Spring Boot BrainboxApi scaffold

---
+++ src/main/kotlin/com/Afrithecus/BrainboxApi/BrainboxApiApplication.kt
@@ -0,0 +1,11 @@
+package com.Afrithecus.BrainboxApi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BrainboxApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<BrainboxApiApplication>(*args)
+}
